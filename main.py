05a74f4ddaa0adb6da6d7823e55df8f5c7a386f6
Om Kakatkar <68369994+OmKakatkar@users.noreply.github.com>
2020-09-27 22:12:47 +0530

    major update
    Refactoring the game loop code into smaller functions for easy debugging
    Yeah, some bugs may have been introduced.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import pygame
+# flake8: noqa = E501
+
 import random
 import os
 import sys
 
-pygame.init()
-pygame.font.init()
-
-screen_width = 450
-screen_height = 400
+from pygame.locals import *
+import pygame
 
 # Window creation
+pygame.init()
+pygame.font.init()
+screen_width = 450
+screen_height = 400
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Snake")
 icon = pygame.image.load("data/images/snake.png")
@@ -17,6 +19,7 @@ pygame.display.set_icon(icon)
 
 # Start the clock
 clock = pygame.time.Clock()
+fps = 30
 
 # Font Styles
 font_large = pygame.font.SysFont('comicsansms', 45)
@@ -24,40 +27,104 @@ font_normal = pygame.font.SysFont('comicsansms', 30)
 font_small = pygame.font.SysFont('comicsansms', 22)
 
 # Colors
-white = (255, 255, 255)
-black = (0, 0, 0)
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGRAY = (50, 50, 50)
+GRAY = (192, 192, 192)
+RED = (220, 0, 0)
+GREEN = (0, 200, 0)
+BLUE = (0, 0, 255)
 
-# Renders text on the pygame surface
+# Helper Functions
 
 
 def text_write(text, color, x_pos, y_pos, font_set=font_normal):
     screen_text = font_set.render(text, True, color)
     screen.blit(screen_text, [x_pos, y_pos])
 
-# Draws the snake
-
 
 def draw_snake(surface, color, snake_pos_list, snake_size,):
     for x_pos, y_pos in snake_pos_list:
-        pygame.draw.rect(screen, black, [x_pos, y_pos, snake_size, snake_size])
+        pygame.draw.rect(screen, BLACK, [x_pos, y_pos, snake_size, snake_size])
 
 
 def draw_food(surface, color, food_x, food_y, size):
     pygame.draw.rect(
-        screen, red, [food_x, food_y, size, size])
+        screen, RED, [food_x, food_y, size, size])
 
 
-def game_loop():
-    # Game Specific Variables
+def pause():
     running = True
-    game_state = "Home"
+    while running:
+
+        # screen.fill((GRAY))
+        text_write("Paused", RED, 150, 150, font_large)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+        clock.tick(5)
+
+
+# Screens
+
+def main_menu():
+
+    clicked = False
+    while True:
+        screen.fill(GRAY)
+        text_write("Welcome to Snakes", RED, 20, 80, font_large)
+
+        button_start = pygame.Rect(84, 223, 96, 45)
+        button_quit = pygame.Rect(270, 223, 96, 45)
+
+        mx, my = pygame.mouse.get_pos()
+
+        # Check collision of Mouse and Button
+        if button_start.collidepoint((mx, my)):
+            pygame.draw.rect(screen, (RED), button_start)
+            if clicked:
+                game()
+        else:
+            pygame.draw.rect(screen, (DARKGRAY), button_start)
+        if button_quit.collidepoint((mx, my)):
+            pygame.draw.rect(screen, (RED), button_quit)
+            if clicked:
+                pygame.quit()
+                sys.exit()
+        else:
+            pygame.draw.rect(screen, (DARKGRAY), button_quit)
+
+        text_write("Start", GREEN, 92, 223)
+        text_write("Quit", GREEN, 285, 223)
+
+        # Event Handling
+        clicked = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    clicked = True
+
+        pygame.display.update()
+        clock.tick(fps)
+
+
+def game():
+    # Game Screen
+
+    #  Game Specific Variables
+    running = True
     score = 0
-    border = 75
     padding = 10
-    fps = 30
     score_bar_y_pos = 50
     if(not os.path.exists("high_score.txt")):
         with open("high_score.txt", 'w') as f:
@@ -82,131 +149,166 @@ def game_loop():
     food_y = random.randrange(
         score_bar_y_pos, screen_height + 1 - snake_size, 10)
 
-    # Game Loop
     while running:
 
-        # Home Screen
-        if game_state == "Home":
-            screen.fill(white)
-            text_write("Welcome to Snakes", red, 20, 80, font_large)
-            text_write("Press Enter", red, 100, 160, font_large)
-            text_write("To Start", red, 120, 240, font_large)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
+        # Score update if food eaten
+        if abs(snake_x - food_x) < 8 and abs(snake_y - food_y) < 8:
+            score += 10
+            snake_length += 1
+            food_x = random.randrange(
+                0, screen_width-snake_size + 1 - padding, 10)
+            food_y = random.randrange(
+                score_bar_y_pos, screen_height + 1 - snake_size, 10)
 
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN:
-                        game_state = "Running"
+            if score > high_score:
+                high_score = score
 
-        # Exit Screen
-        elif game_state == "Game-Over":
-            screen.fill(white)
-            text_write('Game Over', red, 120, 80, font_large)
-            text_write('Press "Enter"', red, 100, 150, font_large)
-            text_write('To Continue...', red, 100, 220, font_large)
-            with open("high_score.txt", 'w') as f:
-                f.write(str(high_score))
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
+        # Set display properties
+        screen.fill(GRAY)
+        pygame.draw.rect(
+            screen, DARKGRAY, [0, 0, screen_width, score_bar_y_pos])
 
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN:
-                        game_state = "Home"
-                        game_loop()
+        # Display Score
+        text_write("Score : " + str(score), GREEN,
+                   5, 5)
+        text_write("   High Score : " + str(high_score), GREEN,
+                   180, 5)
 
-        # Game Screen
-        elif game_state == "Running":
+        # Snake's current position
+        snake_head = []
+        snake_head.append(snake_x)
+        snake_head.append(snake_y)
 
-            # Event Handling
-            events = pygame.event.get()
-            for event in events:
-                if event.type == pygame.QUIT:
-                    running = False
+        snake_pos_list.append(snake_head)
 
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP and snake_direction != 'DOWN':
-                        snake_direction = 'UP'
-                        velocity_x = 0
-                        velocity_y = -init_velocity
-                    if event.key == pygame.K_DOWN and snake_direction != 'UP':
-                        snake_direction = 'DOWN'
-                        velocity_x = 0
-                        velocity_y = init_velocity
-                    if event.key == pygame.K_LEFT and snake_direction != 'RIGHT':
-                        snake_direction = 'LEFT'
-                        velocity_x = -init_velocity
-                        velocity_y = 0
-                    if event.key == pygame.K_RIGHT and snake_direction != 'LEFT':
-                        snake_direction = 'RIGHT'
-                        velocity_x = init_velocity
-                        velocity_y = 0
+        # Delete old heads
+        if len(snake_pos_list) > snake_length:
+            del snake_pos_list[0]
 
-            # Score update if food eaten
-            if abs(snake_x - food_x) < 8 and abs(snake_y - food_y) < 8:
-                score += 10
-                snake_length += 1
-                food_x = random.randrange(
-                    0, screen_width-snake_size + 1 - padding, 10)
-                food_y = random.randrange(
-                    score_bar_y_pos, screen_height + 1 - snake_size, 10)
+        # Self collision for snake
+        if snake_head in snake_pos_list[:-1]:
+            game_end(high_score)
+            break
 
-                if score > high_score:
-                    high_score = score
+        # Border Checking
+        if snake_x < 0:
+            snake_x = 0
+            game_end(high_score)
+            break
+        elif snake_x > screen_width - snake_size:
+            snake_x = screen_width - snake_size
+            game_end(high_score)
+            break
+        if snake_y < score_bar_y_pos:
+            snake_y = 0
+            game_end(high_score)
+            break
+        elif snake_y > screen_height - snake_size:
+            snake_y = screen_height - snake_size
+            game_end(high_score)
+            break
+        
+        # Draw particles
+        draw_snake(screen, DARKGRAY, snake_pos_list, snake_size)
+        draw_food(screen, RED, food_x, food_y, snake_size)
 
-                    # Display Settings
-            screen.fill(white)
-            # Score bar background
-            pygame.draw.rect(
-                screen, black, [0, 0, screen_width, score_bar_y_pos])
+        # Handling snake movement
+        snake_x += velocity_x
+        snake_y += velocity_y
 
-            # Print Score on screen
-            text_write("Score : " + str(score), green,
-                       5, 5)
-            text_write("   High Score : " + str(high_score), green,
-                       180, 5)
-
-            # Snake's current position
-            snake_head = []
-            snake_head.append(snake_x)
-            snake_head.append(snake_y)
-
-            snake_pos_list.append(snake_head)
-
-            # Delete old heads
-            if len(snake_pos_list) > snake_length:
-                del snake_pos_list[0]
-
-            # Self collision for snake
-            if snake_head in snake_pos_list[:-1]:
-                game_state = "Game-Over"
-
-            # Border Checking
-            if snake_x < 0:
-                snake_x = 0
-                game_state = "Game-Over"
-            elif snake_x > screen_width - snake_size:
-                snake_x = screen_width - snake_size
-                game_state = "Game-Over"
-            if snake_y < score_bar_y_pos:
-                snake_y = 0
-                game_state = "Game-Over"
-            elif snake_y > screen_height - snake_size:
-                snake_y = screen_height - snake_size
-                game_state = "Game-Over"
-
-            draw_snake(screen, black, snake_pos_list, snake_size)
-            draw_food(screen, red, food_x, food_y, snake_size)
-
-            snake_x += velocity_x
-            snake_y += velocity_y
+        # Event Handling
+        events = pygame.event.get()
+        for event in events:
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_UP and snake_direction != 'DOWN':
+                    snake_direction = 'UP'
+                    velocity_x = 0
+                    velocity_y = -init_velocity
+                if event.key == K_DOWN and snake_direction != 'UP':
+                    snake_direction = 'DOWN'
+                    velocity_x = 0
+                    velocity_y = init_velocity
+                if event.key == K_LEFT and snake_direction != 'RIGHT':
+                    snake_direction = 'LEFT'
+                    velocity_x = -init_velocity
+                    velocity_y = 0
+                if event.key == K_RIGHT and snake_direction != 'LEFT':
+                    snake_direction = 'RIGHT'
+                    velocity_x = init_velocity
+                    velocity_y = 0
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pause()
 
         pygame.display.update()
         clock.tick(fps)
 
-    pygame.quit()
-    sys.exit()
 
+def game_end(high_score):
+    clicked = False
+    running = True
+    while running:
+        screen.fill(GRAY)
+        text_write('Game Over', RED, 120, 60, font_large)
 
-game_loop()
+        # Saving high Score
+        with open("high_score.txt", 'w') as f:
+            f.write(str(high_score))
+
+        button_restart = pygame.Rect(175, 150, 100, 45)
+        button_menu = pygame.Rect(175, 220, 100, 45)
+        button_quit = pygame.Rect(175, 290, 100, 45)
+
+        mx, my = pygame.mouse.get_pos()
+
+        # Restart Button
+        if button_restart.collidepoint((mx, my)):
+            pygame.draw.rect(screen, (RED), button_restart)
+            if clicked:
+                running = False
+                game()
+                break
+        else:
+            pygame.draw.rect(screen, (DARKGRAY), button_restart)
+
+        # Menu Bottom
+        if button_menu.collidepoint((mx, my)):
+            pygame.draw.rect(screen, (RED), button_menu)
+            if clicked:
+                running = False
+                break
+        else:
+            pygame.draw.rect(screen, (DARKGRAY), button_menu)
+
+        # Quit Button
+        if button_quit.collidepoint((mx, my)):
+            pygame.draw.rect(screen, (RED), button_quit)
+            if clicked:
+                pygame.quit()
+                sys.exit()
+        else:
+            pygame.draw.rect(screen, (DARKGRAY), button_quit)
+
+        text_write("Restart", GREEN, 185, 155, font_small)
+        text_write("Menu", GREEN, 195, 225, font_small)
+        text_write("Quit", GREEN, 200, 295, font_small)
+
+        # Event Handling
+        clicked = False
+        events = pygame.event.get()
+        for event in events:
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    clicked = True
+
+        pygame.display.update()
+        clock.tick(fps)
+
+# Entry Point
+main_menu()
